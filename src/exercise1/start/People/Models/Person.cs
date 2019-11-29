9940d61eb553570fc6234c08685a6b4aec031871
studentsimone <55529034+studentsimone@users.noreply.github.com>
2019-11-29 11:38:59 +0200

    sqlite-assessment

---
+++ src/exercise1/start/People/Models/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace People.Models
+{
+   public class Person 
+    {
+        public string Name { get; internal set; }
+
+        [Table("people")]
+        public class person
+        {
+            [PrimaryKey, AutoIncrement]
+            public int Id { get; set; }
+
+            [MaxLength(250), Unique]
+            public string Name { get; set; }
+        }
+
+    }
+}
